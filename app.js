3c9b4aef40fc44d9a05e13fa29993e7292718b66
Tatiana Kudria <94138737+TatianaKud@users.noreply.github.com>
2022-10-04 19:47:16 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,77 +1,87 @@
-const startBtn = document.querySelector('#start')
-const screens = document.querySelectorAll('.screen')
-const timeList = document.querySelector('#time-list')
-const timeEl = document.querySelector('#time')
-const board = document.querySelector('#board')
+const startBtn = document.querySelector("#start")
+const screens = document.querySelectorAll(".screen")
+const timeList = document.querySelector("#time-list")
+const timeEl = document.querySelector("#time")
+const board = document.querySelector("#board")
 
 let time = 0
 let score = 0
 
-startBtn.addEventListener('click', (event) => {
-    event.preventDefault()
-    screens[0].classList.add('up')
+startBtn.addEventListener("click", (event) => {
+  event.preventDefault()
+  screens[0].classList.add("up")
 })
 
-timeList.addEventListener('click', event => {
-    if (event.target.classList.contains('time-btn')) {
-        time = parseInt(event.target.getAttribute('data-time'))
-        screens[1].classList.add('up')
-        startGame()
-    }
+timeList.addEventListener("click", (event) => {
+  if (event.target.classList.contains("time-btn")) {
+    time = parseInt(event.target.getAttribute("data-time"))
+    screens[1].classList.add("up")
+    startGame()
+  }
 })
 
-board.addEventListener('click', event => {
-    if (event.target.classList.contains('circle')) {
-        score++
-        event.target.remove()
-        createRandomCircle()
-    }
+board.addEventListener("click", (event) => {
+  if (event.target.classList.contains("circle")) {
+    score++
+    event.target.remove()
+    createRandomCircle()
+  }
 })
-
 
 function startGame() {
-    setInterval(decreaseTime, 1000)
-    createRandomCircle()
-    setTime(time)
+  setInterval(decreaseTime, 1000)
+  createRandomCircle()
+  setTime(time)
 }
 
 function decreaseTime() {
-    if(time === 0) {
-        finishGame()
-    } else {
-        let current = --time
-        if (current < 10) {
-            current = `0${current}`
+  if (time === 0) {
+    finishGame()
+  } else {
+    let current = --time
+    if (current < 10) {
+      current = `0${current}`
     }
     setTime(current)
-    }
+  }
 }
 
 function setTime(value) {
-    timeEl.innerHTML = `00:${value}`
+  timeEl.innerHTML = `00:${value}`
 }
 
 function finishGame() {
-    timeEl.parentNode.classList.add('hide')
-    board.innerHTML = `<h1>Cчет: <span class="primary">${score}</span></h1>`
+  timeEl.parentNode.classList.add("hide")
+  board.innerHTML = `<h1>Cчет: <span class="primary">${score}</span></h1>`
 }
 
 function createRandomCircle() {
-    const circle = document.createElement('div')
-    const size = getRandomeNumber(10, 60)
-    const {width, height} = board.getBoundingClientRect()
-    const x = getRandomeNumber(0, width - size)
-    const y = getRandomeNumber(0, height - size)
+  const circle = document.createElement("div")
+  const size = getRandomeNumber(10, 60)
+  const { width, height } = board.getBoundingClientRect()
+  const x = getRandomeNumber(0, width - size)
+  const y = getRandomeNumber(0, height - size)
 
-    circle.classList.add('circle')
-    circle.style.width = `${size}px`
-    circle.style.height = `${size}px`
-    circle.style.top = `${y}px`
-    circle.style.left = `${x}px`
+  circle.classList.add("circle")
+  circle.style.width = `${size}px`
+  circle.style.height = `${size}px`
+  circle.style.top = `${y}px`
+  circle.style.left = `${x}px`
 
-    board.append(circle)
+  board.append(circle)
 }
 
 function getRandomeNumber(min, max) {
-    return Math.round(Math.random() * (max - min) + min)
+  return Math.round(Math.random() * (max - min) + min)
+}
+
+function winTheGame() {
+  function kill() {
+    const circle = document.querySelector(".circle")
+
+    if (circle) {
+      circle.click()
+    }
+  }
+  setInterval(kill, 42)
 }
